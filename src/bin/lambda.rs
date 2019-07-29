99f24ded780634340820808217bd0f434991f830
Graham Esau <graham.esau@newvoicemedia.com>
2019-07-30 00:00:51 +0100

    Add binary content example
    Docs need updating!

--- src/bin/lambda.rs
+++ src/bin/lambda.rs
@@ -1,9 +1,10 @@
 use example_rocket_lamb_api;
-use rocket_lamb::{lambda, RocketHandler};
 use rocket::error::LaunchError;
+use rocket_lamb::{lambda, ResponseType, RocketHandler};
 
 fn main() -> Result<(), LaunchError> {
-    let handler = RocketHandler::new(example_rocket_lamb_api::rocket())?;
+    let handler = RocketHandler::new(example_rocket_lamb_api::rocket())?
+        .response_type("image/png", ResponseType::Binary);
     lambda!(handler);
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,26 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::Rocket;
+#[macro_use]
+extern crate rust_embed;
+
+use rocket::{http::ContentType, response::Content, Rocket};
 
 #[get("/")]
 fn hello() -> &'static str {
     "Hello world"
 }
 
+#[derive(RustEmbed)]
+#[folder = "static/"]
+struct StaticFiles;
+
+#[get("/pic")]
+fn pic() -> Option<Content<Vec<u8>>> {
+    let bytes = StaticFiles::get("rocket.png")?.into_owned();
+    Some(Content(ContentType::new("image", "png"), bytes))
+}
+
 pub fn rocket() -> Rocket {
-    rocket::ignite().mount("/", routes![hello])
+    rocket::ignite().mount("/", routes![hello, pic])
 }
