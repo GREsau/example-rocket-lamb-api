64f5e544171630f5d07de18c55d7cab25cd7f964
Graham Esau <gesau@hotmail.co.uk>
2019-09-01 17:54:55 +0100

    Use latest rocket_lamb
    Setting image/png response type to binary is no longer required.

--- src/bin/lambda.rs
+++ src/bin/lambda.rs
@@ -1,9 +1,6 @@
 use example_rocket_lamb_api;
-use rocket_lamb::{ResponseType, RocketExt};
+use rocket_lamb::RocketExt;
 
 fn main() {
-    example_rocket_lamb_api::rocket()
-        .lambda()
-        .response_type("image/png", ResponseType::Binary)
-        .launch();
+    example_rocket_lamb_api::rocket().lambda().launch();
 }
